c214a5356f44b0cb378b0449e585f92465e4eaf7
Rytis Bankieta <rytis.bankieta@gmail.com>
2019-12-19 14:32:23 +0200

    Remove commented coded from Client.hs

--- src/Client.hs
+++ src/Client.hs
@@ -13,9 +13,7 @@ httpSendMessage message player gameId = do
             $ setRequestBody (RequestBodyBS $ BS.pack message)
             $ httpBaseRequest player gameId
     response <- httpBS request
-    -- putStrLn("sendMessage: " ++ gameId ++ "/" ++ player ++ " " ++ message)
     let code = getResponseStatusCode response
-    -- putStrLn("answer:" ++ (BS.unpack $ getResponseBody response))
     return code
 
 httpGetMessage :: String -> String -> IO (Int, String)
@@ -26,7 +24,6 @@ httpGetMessage player gameId = do
             $ httpBaseRequest player gameId
     response <- httpBS request
     let code = getResponseStatusCode response
-    -- putStrLn $ BS.unpack $ getResponseBody response
     return (code, BS.unpack $ getResponseBody response)
 
 httpBaseRequest :: String -> String -> Request
